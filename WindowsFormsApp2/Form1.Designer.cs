fa4430b353732fa24a29861e9469f209a1a2b903
swdev.smz <swdev.smz@gmail.com>
2018-01-07 10:25:54 +0900

    aaa

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.c1DateEdit1 = new C1.Win.Calendar.C1DateEdit();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.c1DateEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1DateEdit1
+            // 
+            this.c1DateEdit1.AllowSpinLoop = false;
+            this.c1DateEdit1.Calendar.RightToLeft = System.Windows.Forms.RightToLeft.Inherit;
+            this.c1DateEdit1.ImagePadding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.c1DateEdit1.Location = new System.Drawing.Point(12, 73);
+            this.c1DateEdit1.Name = "c1DateEdit1";
+            this.c1DateEdit1.Size = new System.Drawing.Size(200, 17);
+            this.c1DateEdit1.TabIndex = 0;
+            this.c1DateEdit1.Tag = null;
+            this.c1DateEdit1.ValueChanged += new System.EventHandler(this.c1DateEdit1_ValueChanged);
+            this.c1DateEdit1.TextChanged += new System.EventHandler(this.c1DateEdit1_TextChanged);
+            this.c1DateEdit1.LostFocus += new System.EventHandler(this.c1DateEdit1_LostFocus);
+
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.c1DateEdit1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.c1DateEdit1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private C1.Win.Calendar.C1DateEdit c1DateEdit1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -12,9 +12,84 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+
+        public class Sample : INotifyPropertyChanged
+        {
+            public String _aaa;
+            public String Aaa
+            {
+                set
+                {
+                    this._aaa = value;
+                    OnPropertyChanged("Aaa");
+                }
+                get
+                {
+                    return this._aaa;
+                }
+                
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            // Create the OnPropertyChanged method to raise the event
+            protected void OnPropertyChanged(string name)
+            {
+                PropertyChangedEventHandler handler = PropertyChanged;
+                if (handler != null)
+                {
+                    handler(this, new PropertyChangedEventArgs(name));
+                }
+            }
+        }
+
+        private Sample abc = new Sample();
+        private double i = 1;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            c1DateEdit1.DataSource = abc;
+            c1DateEdit1.DataField = "Aaa";
+            */
+            c1DateEdit1.DataBindings.Add("Value", abc,"Aaa");
+
+            abc.Aaa = DateTime.Now.ToString();
+
+
+        }
+
+        private void c1DateEdit1_ValueChanged(object sender, EventArgs e)
+        {
+           // Console.WriteLine(abc.Aaa);
+        }
+        
+
+        private void c1DateEdit1_TextChanged(object sender, EventArgs e)
+        {
+            //Console.WriteLine(abc.Aaa);
+        }
+
+
+        private void c1DateEdit1_LostFocus(object sender, EventArgs e)
+        {
+            Console.WriteLine(abc.Aaa);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            abc.Aaa = DateTime.Now.AddDays(i).ToString();
+            i++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            abc.Aaa = null;
         }
     }
 }
